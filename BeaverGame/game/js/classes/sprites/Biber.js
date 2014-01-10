d02c42405fb32dede15715a4bf76c977c55338cd
jongminpark <jmpaaak@naver.com>
2014-01-10 18:36:40 +0900

    move functions

--- BeaverGame/game/js/classes/sprites/Biber.js
+++ BeaverGame/game/js/classes/sprites/Biber.js
@@ -10,13 +10,38 @@ classes.sprites.Biber = cc.Sprite.extend({
 	    },
 	    handleKey:function(e)
 	    {
+	    	var that = this;
 	        if(e === cc.KEY.left)
 	        {
 	            this._currentRotation--;
+	            this.runAction(cc.Sequence.create(
+            		cc.MoveBy.create(0.01, cc.p(-10, 0)),
+            		cc.CallFunc.create(function() {
+            			that.setScaleX(0.5);
+	    				that.setScaleY(2.0);
+            		}),
+            		cc.DelayTime.create(0.5),
+	    			cc.CallFunc.create(this.beFreak, this)
+           		));
+           		this.setScaleX(0.5);
+	    		this.setScaleY(2.0);
 
 	        }
 	        else if(e === cc.KEY.right)
+	        {
 	            this._currentRotation++;
+	            this.runAction(cc.Sequence.create(
+            		cc.MoveBy.create(0.01, cc.p(10, 0)),
+            		cc.CallFunc.create(function() {
+            			that.setScaleX(0.5);
+	    				that.setScaleY(2.0);
+            		}),
+            		cc.DelayTime.create(0.5),
+	    			cc.CallFunc.create(this.beFreak, this)
+           		));
+           		this.setScaleX(0.5);
+	    		this.setScaleY(2.0);
+			}	            
 
 	        if(this._currentRotation < 0) this._currentRotation = 360;
 	        if(this._currentRotation > 360) this._currentRotation = 0;
@@ -35,5 +60,9 @@ classes.sprites.Biber = cc.Sprite.extend({
 	        angle = angle * (180/Math.PI);
 	        this._currentRotation = angle;
 
+	    },
+	    beFreak: function() {
+	    	this.setScaleX(1.0);
+	    	this.setScaleY(1.0);
 	    }
 });
